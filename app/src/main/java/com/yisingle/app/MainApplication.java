0390d2a4c7a755c26fb1c07cc0887694d301505f
JIKUN <j314815101@qq.com>
2017-06-13 18:41:47 +0800

    优化代码   突破65000方法数限制

--- app/src/main/java/com/yisingle/app/MainApplication.java
+++ app/src/main/java/com/yisingle/app/MainApplication.java
@@ -1,8 +1,11 @@
 package com.yisingle.app;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.orhanobut.logger.Logger;
+import com.tencent.bugly.crashreport.CrashReport;
 
 /**
  * Created by jikun on 17/5/12.
@@ -12,10 +15,18 @@ public class MainApplication extends Application {
 
 
     @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+    @Override
     public void onCreate() {
         super.onCreate();
         AppManager.getInstance().init(this);
         Logger.init().methodCount(0).hideThreadInfo();
+
+        CrashReport.initCrashReport(getApplicationContext(), "8856cf1479", false);
 
     }
 }
